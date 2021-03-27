b53ea20792b695074fb07313de1aa1c4607acf19
Akanksha Kakar <34072193+Akanksha0315@users.noreply.github.com>
2021-03-27 17:41:19 +0530

    1. Added Tab Bar Controller 2. Added and Styled Hosting Schedule Screen and Attending Schedule Screen to Main.storyboard 3. Added new CocoaTouch files titled HostingScheduleViewController.swift and AttendingScheduleViewController.swift 4. Associated Hosting Schedule Screen in Main.storyboard HostingScheduleViewController.swift 5. Created Required Outlets HostingScheduleViewController.swift file 6. Associated Attending Schedule Screen in Main.storyboard AttendingScheduleViewController.swift 7. Created Required Outlets AttendingScheduleViewController.swift file

---
+++ Converse/AttendingScheduleViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AttendingScheduleViewController.swift
+//  Converse
+//
+//  Created by Akku on 3/27/21.
+//
+
+import UIKit
+
+class AttendingScheduleViewController: UIViewController {
+    
+    @IBOutlet weak var scheduleLabel: UILabel!
+    @IBOutlet weak var attendingScheduleTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
