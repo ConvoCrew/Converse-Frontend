f083a2198a140261651bca2333c0ae7972e0f1dd
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-08-20 22:16:27 +0200

    Full Support for SwiftUI

---
+++ Converse/SwiftUI Views/WelcomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePageView.swift
+//  Converse
+//
+//  Created by Cédric Bahirwe on 20/08/2021.
+//
+
+import SwiftUI
+
+struct WelcomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomePageView()
+    }
+}
