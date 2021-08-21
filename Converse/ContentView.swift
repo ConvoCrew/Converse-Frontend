eb18fa6be55106a02d219899da0bc7c9339921d5
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-08-21 22:06:05 +0200

    TabBarView added

--- Converse/ContentView.swift
+++ Converse/ContentView.swift
@@ -9,28 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @AppStorage("selectedTab")
+    private var selectedTab: Int = 1
+    
     var body: some View {
-        TabView {
-            
-            CreateRoomView()
-                .tabItem {
-                    Label("Create Room", systemImage: "house")
-                }
-            
-            ExplorePageView()
-                .tabItem {
-                    Label("Explore Room", systemImage: "book")
-                }
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                
+                CreateRoomView()
+                    .tabItem {
+                        Label("Create Room", systemImage: "house")
+                    }
+                    .tag(0)
+                
+                ExplorePageView()
+                    .tabItem {
+                        Label("Explore Room", systemImage: "book")
+                    }
+                    .tag(1)
 
-            SchedulePageView()
-                .tabItem {
-                    Label("Schedule Room", systemImage: "alarm")
-                }
+                SchedulePageView()
+                    .tabItem {
+                        Label("Schedule Room", systemImage: "alarm")
+                    }
+                    .tag(2)
+                
+                ProfilePageview()
+                    .tabItem {
+                        Label("Profile", systemImage: "person")
+                    }
+                    .tag(3)
+            }
             
-            ProfilePageview()
-                .tabItem {
-                    Label("Profile", systemImage: "person")
+            
+//            GeometryReader { _ in
+                VStack {
+                    Spacer()
+                    TabBarView(selection: $selectedTab)
                 }
+                .ignoresSafeArea()
         }
     }
 }
